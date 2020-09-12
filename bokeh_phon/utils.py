1fcc319c43f8f39317727cba06ada86315f1362e
Ronald Sprouse <ronald@berkeley.edu>
2020-09-11 17:04:06 -0700

    Check base_url for separator

--- bokeh_phon/utils.py
+++ bokeh_phon/utils.py
@@ -42,6 +42,8 @@ def remote_jupyter_proxy_url_callback(port):
     service_url_path = os.environ['JUPYTERHUB_SERVICE_PREFIX']
     proxy_url_path = 'proxy/%d' % port
 
+    if not base_url.endswith('/'):
+        base_url += '/'
     user_url = urllib.parse.urljoin(base_url, service_url_path)
     full_url = urllib.parse.urljoin(user_url, proxy_url_path)
     return full_url
